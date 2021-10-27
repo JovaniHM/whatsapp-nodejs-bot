cea3746ebb1f37fe9778bcc9ffcc45f7f7d46b22
JovaniHM <jovani.hernandez@vlim.com.mx>
2021-10-27 11:55:55 -0500

    fixed bot message

--- index.js
+++ index.js
@@ -77,15 +77,17 @@ app.post('/webhook', async function (req, res) {
             });
         }
 
-        const caseValue = ( (body == 'Hola' || body == 'hola' || body == 'Hello' || body == 'hello' || body == 'Hi' || body == 'hi') ? body : ( guests[ index ].clave + body ) );
-
         const query = connection.query(`CALL strGetInfoGuestWhatsApp( '${ phone }' );`);
 
         query.on('result', async function(row, index) {
             if ( row.BAN == 1 ) {
                 if ( row.REGISTROS_INGLES == 0 ) {
+                    const caseValue = ( (body == 'Hola' || body == 'hola') ? 'Hola' : ( guests[ index ].clave + body ) );
+
                     await es_bot(caseValue, guests, index, text, chatId, row, author, chatName);
                 } else {
+                    const caseValue = ( (body == 'Hello' || body == 'hello' || body == 'Hi' || body == 'hi') ? 'Hello' : ( guests[ index ].clave + body ) );
+
                     await en_bot(caseValue, guests, index, text, chatId, row, author, chatName)
                 }
             }
